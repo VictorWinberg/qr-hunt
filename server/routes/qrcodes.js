9a734549b153da2a1ed41dcca2b04ddf3d909543
Annie Onnered <annie.onnered@gmail.com>
2021-01-17 15:01:03 +0100

    Change link for api fetch

--- server/routes/qrcodes.js
+++ server/routes/qrcodes.js
@@ -3,14 +3,14 @@ const { v4: uuidv4 } = require("uuid");
 module.exports = ({ app, pg, isLoggedIn }) => {
   const QRCode = require("../models/qrcode")(pg);
 
-  app.get("/__/qrcodes", (_, res) => {
+  app.get("/api/qrcodes", (_, res) => {
     QRCode.getAll((err, qrcodes) => {
       if (err) return res.status(400).send(err);
       return res.send(qrcodes);
     });
   });
 
-  app.post("/__/qrcodes", (_, res) => {
+  app.post("/api/qrcodes", (_, res) => {
     QRCode.create({ uuid: uuidv4() }, (err, qrcode) => {
       if (err) return res.status(400).send(err);
       return res.send(qrcode);
