97cb34077f8a5e26f56c59a87acbe00ee86eae51
Victor Winberg <victor.winberg@jayway.com>
2021-01-24 09:42:41 +0100

    Update eslint package versions

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,34 +1,17 @@
 module.exports = {
   env: {
-    node: true
+    node: true,
   },
   extends: [
+    "plugin:vue/essential",
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:vue/recommended",
-    "@vue/standard",
-    "@vue/typescript",
-    "@vue/prettier"
+    "@vue/typescript/recommended",
   ],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
   parserOptions: {
-    parser: "@typescript-eslint/parser",
-    project: "./tsconfig.json",
-    tsconfigRootDir: __dirname
+    ecmaVersion: 2020,
   },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
 };
