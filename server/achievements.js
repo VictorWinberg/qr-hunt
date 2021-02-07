8df1189901f8d0f2c5006add7b3d9165b0903491
Victor Winberg <victor.winberg@jayway.com>
2021-02-07 22:59:19 +0100

    Add achievement badges

--- server/achievements.js
+++ server/achievements.js
@@ -1,33 +1,41 @@
 const achievements = {
-  USER_HAS_SIGNED_UP: async ({ key, db, req }) => {
-    if (req.path !== "/auth/google/callback" || !req.user) return;
-    const sql = `
-        INSERT INTO user_achievements (user_id, achievement_name) VALUES ($1, $2)
-        ON CONFLICT DO NOTHING RETURNING *`;
-
-    const { err } = await db.query(sql, [req.user.id, key]);
-    if (err) console.error(err);
+  USER_HAS_SIGNED_UP: async ({ req }) => {
+    return req.path === "/auth/google/callback" && req.user;
   },
+  FIRST_QRSPOT_FOUND: async ({ req, res }) => {
+    return (
+      (req.route || {}).path === "/api/scan/:id" &&
+      req.user &&
+      (res.body || {}).qrspot
+    );
+  }
 };
 
 module.exports = ({ pg, db }) => async (req, res, next) => {
   var fnJson = res.json.bind(res);
-  res.json = (json) => {
+  res.json = json => {
     res.body = json;
     fnJson.call(res, json);
   };
 
   res.on("finish", async () => {
-    Object.entries(achievements).forEach(([key, fn]) =>
-      fn({ key, pg, db, req, res })
-    );
+    Object.entries(achievements).forEach(async ([key, fn]) => {
+      if (await fn({ key, pg, db, req, res })) {
+        const sql = `
+          INSERT INTO user_achievements (user_id, achievement_name) VALUES ($1, $2)
+          ON CONFLICT DO NOTHING RETURNING *`;
+
+        const { err } = await db.query(sql, [req.user.id, key]);
+        if (err) console.error(err);
+      }
+    });
   });
 
   if (req.url === "/api/achievements") {
     if (!req.isAuthenticated()) return res.sendStatus(401);
 
     const sql = `
-        SELECT achievement_name as name, title, score, level, user_achievements.created_at
+        SELECT achievement_name as name, title, icon, score, level, user_achievements.created_at
         FROM user_achievements
         FULL JOIN achievements ON user_achievements.achievement_name = achievements.name
         WHERE user_id = $1`;
