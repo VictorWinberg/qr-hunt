e17f17fa2a4da0ad6e7ad68fb0b6304f6f81547f
Victor Winberg <victor.m.winberg@gmail.com>
2021-02-27 18:34:17 +0100

    Fix snackbar (#82)

--- client/src/plugins/achievements.ts
+++ client/src/plugins/achievements.ts
@@ -3,35 +3,13 @@ import store from "@/store";
 import { api } from "@/utils";
 
 (async () => {
-  const thankful = await api.get("/api/achievements/thankful");
-  if (!thankful.err && !thankful.data) {
-    Snackbar.show({
-      text: "Welcome to QR-Hunt!",
-      actionText: "Thanks!",
-      actionTextColor: "#f66496",
-      pos: "top-center",
-      onActionClick: () => {
-        Snackbar.show({
-          text: "YEEEY! You are so kind!",
-          textColor: "#f66496",
-          pos: "top-center",
-          showAction: false
-        });
-        api.post("/api/achievements/thankful");
-      }
-    });
-  }
-
   const newAchievements = async () => {
-    // @ts-ignore
-    if (!store.state.auth.isAuthenticated) return;
-
     const achievement = await api.get("/api/achievements/new");
     if (!achievement.err && achievement.data) {
       const { name, title } = achievement.data;
       Snackbar.show({
-        text: `<b>Achievement:</b> ${title || name}`,
-        actionText: "Collect!",
+        text: `<b>-Achievement-</b><br/> ${title || name}`,
+        actionText: "<i class='fas fa-hand-holding-heart fa-2x'></i>",
         actionTextColor: "#32a852",
         duration: 10 * 1000,
         pos: "top-center",
@@ -50,11 +28,36 @@ import { api } from "@/utils";
           }
           // @ts-ignore
           Snackbar.close();
+
+          setTimeout(newAchievements, 3 * 1000);
         }
       });
     }
-    setTimeout(newAchievements, 10 * 1000);
+
+    setTimeout(newAchievements, 30 * 1000);
   };
 
   newAchievements();
+
+  const thankful = await api.get("/api/achievements/thankful");
+  if (!thankful.err && !thankful.data) {
+    Snackbar.show({
+      text: "Welcome to QR-Hunt!",
+      actionText: "Thanks! <i class='fas fa-comment'></i>",
+      actionTextColor: "#f66496",
+      pos: "top-center",
+      onActionClick: async () => {
+        Snackbar.show({
+          text: `<b>Yeeey!</b><br/> You are so kind!`,
+          textColor: "#f66496",
+          actionText: "<i class='fas fa-heart fa-2x'></i>",
+          actionTextColor: "#f66496",
+          pos: "top-center"
+        });
+        await api.post("/api/achievements/thankful");
+
+        setTimeout(newAchievements, 5 * 1000);
+      }
+    });
+  }
 })();
