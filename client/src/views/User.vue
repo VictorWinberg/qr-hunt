555626759542db8d18107c8c89a82224c35a4b06
Victor Winberg <victor.m.winberg@gmail.com>
2021-05-20 11:14:11 +0200

    Add nullish coalescing operator in User view

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -15,12 +15,12 @@
         :style="{ color: hashColor(user.lvl) }"
       >
         <span class="user-xp__text" :style="xpTextStyle(user.lvl)">
-          {{ user.lvl || "?" }}
+          {{ user.lvl ?? "?" }}
         </span>
       </i>
       <div class="user-xp__bar">
         <span class="user-xp__text" :style="xpTextStyle(user.lvl)">
-          {{ user.lvlXp }} / {{ user.reqLvlXp || "?" }}
+          {{ user.lvlXp }} / {{ user.reqLvlXp ?? "?" }}
         </span>
         <div
           class="user-xp__bar--fill"
