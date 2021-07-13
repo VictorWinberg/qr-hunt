9d267cb103c3aa3d46ccfd450d7281df4fd357ec
Victor Winberg <victor.m.winberg@gmail.com>
2021-07-13 16:48:32 +0200

    Fix achievements

--- client/src/plugins/achievements.ts
+++ client/src/plugins/achievements.ts
@@ -50,27 +50,27 @@ import { api } from "@/utils";
   // Don't show achievements when in intro mode
   if (new URLSearchParams(window.location.search).get("intro")) return;
 
-  setTimeout(newAchievements, 10 * 1000);
+  newAchievements();
 
-  // const thankful = await api.get("/api/achievements/thankful");
-  // if (!thankful.err && !thankful.data) {
-  //   Snackbar.show({
-  //     text: "Welcome to QR-Hunt!",
-  //     actionText: "Thanks! <i class='fas fa-comment'></i>",
-  //     actionTextColor: "#f66496",
-  //     pos: "top-center",
-  //     onActionClick: async () => {
-  //       Snackbar.show({
-  //         text: `<b>Yeeey!</b><br/> You are so kind!`,
-  //         textColor: "#f66496",
-  //         actionText: "<i class='fas fa-heart fa-2x'></i>",
-  //         actionTextColor: "#f66496",
-  //         pos: "top-center"
-  //       });
-  //       await api.post("/api/achievements/thankful");
+  const thankful = await api.get("/api/achievements/thankful");
+  if (!thankful.err && !thankful.data) {
+    Snackbar.show({
+      text: "Welcome to QR-Hunt!",
+      actionText: "Thanks! <i class='fas fa-comment'></i>",
+      actionTextColor: "#f66496",
+      pos: "top-center",
+      onActionClick: async () => {
+        Snackbar.show({
+          text: `<b>Yeeey!</b><br/> You are so kind!`,
+          textColor: "#f66496",
+          actionText: "<i class='fas fa-heart fa-2x'></i>",
+          actionTextColor: "#f66496",
+          pos: "top-center"
+        });
+        await api.post("/api/achievements/thankful");
 
-  //       setTimeout(newAchievements, 5 * 1000);
-  //     }
-  //   });
-  // }
+        setTimeout(newAchievements, 5 * 1000);
+      }
+    });
+  }
 })();
