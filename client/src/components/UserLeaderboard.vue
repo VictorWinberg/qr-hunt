794cff95cdd31f9d5c9e90ebb9dddcab3f9de8ff
Victor Winberg <victor.m.winberg@gmail.com>
2021-07-15 21:51:32 +0200

    Add leaderboard

--- client/src/components/UserLeaderboard.vue
+++ client/src/components/UserLeaderboard.vue
@@ -1,24 +1,65 @@
 <template>
   <div>
-    <h2 class="">Leaderboard</h2>
-    To be defined
+    <h2 class="leaderboard__title">Leaderboard</h2>
+    <table class="leaderboard__table">
+      <tr>
+        <th>Rank</th>
+        <th style="width: 0;"></th>
+        <th>User</th>
+        <th>Lvl</th>
+      </tr>
+      <tr v-for="user in leaderboard" :key="user.id">
+        <td>{{ user.rank }}</td>
+        <td style="width: 0;">
+          <div
+            class="friend__photo"
+            :style="{
+              backgroundImage: `url(${user.photo})`,
+              marginBottom: '-.5em' // Todo: remove this line!
+            }"
+          ></div>
+        </td>
+        <td>{{ user.name || user.username }}</td>
+        <td>{{ user.lvl }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 import { mapState, mapMutations } from "vuex";
+import { EVENT_TYPE } from "@/constants";
 import { api } from "@/utils";
+import EventBus from "@/plugins/event-bus";
 
 export default {
   computed: {
-    ...mapState("friends", ["friends"])
+    ...mapState("user", ["leaderboard"])
   },
-  async created() {
-    const friends = await api.get("/api/users");
-    if (!friends.err) this.setFriends(friends.data);
+  created() {
+    this.fetchLeaderboard();
+    EventBus.$on(EVENT_TYPE.API_REQUEST_UPDATE, this.fetchLeaderboard);
+  },
+  beforeDestroy() {
+    EventBus.$off(EVENT_TYPE.API_REQUEST_UPDATE, this.fetchLeaderboard);
   },
   methods: {
-    ...mapMutations("friends", ["setFriends"])
+    ...mapMutations("user", ["setLeaderboard"]),
+    async fetchLeaderboard() {
+      const leaderboard = await api.get("/api/leaderboard");
+      if (!leaderboard.err) this.setLeaderboard(leaderboard.data);
+    }
   }
 };
 </script>
+
+<style lang="scss">
+.leaderboard__table {
+  min-width: 500px;
+  margin: auto;
+
+  tr {
+    text-align: left;
+  }
+}
+</style>
