fbb11df886551bab122624edaa8671e3e9a04485
Victor Winberg <victor.m.winberg@gmail.com>
2021-09-03 09:30:59 +0200

    Update google-maps.ts

--- client/src/plugins/google-maps.ts
+++ client/src/plugins/google-maps.ts
@@ -3,6 +3,7 @@ import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: process.env.VUE_APP_GOOGLE_API_KEY || ""
+    key: process.env.VUE_APP_GOOGLE_API_KEY || "",
+    v: "beta"
   }
 });
