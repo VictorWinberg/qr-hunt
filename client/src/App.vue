9c1cb278a87f3d3b83c7fd66356266e297992583
Victor Winberg <victor.m.winberg@gmail.com>
2021-09-10 10:43:18 +0200

    App display fixed

--- client/src/App.vue
+++ client/src/App.vue
@@ -38,12 +38,14 @@ export default Vue.extend({
 
 <style lang="scss">
 #app {
-  position: absolute;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 100%;
-  height: 100%;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   color: $text-color;
   text-align: center;
