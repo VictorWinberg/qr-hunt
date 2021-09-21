bd721b389cb5aa93a755e626dfe590d74cb5adcc
Victor Winberg <victor.m.winberg@gmail.com>
2021-09-21 13:50:46 +0200

    Remove sentry tracing lib

--- client/src/plugins/sentry.ts
+++ client/src/plugins/sentry.ts
@@ -2,9 +2,6 @@
 
 import Vue from "vue";
 import * as Sentry from "@sentry/vue";
-import { Integrations } from "@sentry/tracing";
-
-import router from "../router";
 
 const publicKey = "785c1ccde4314c398c91d4b78e9ae3ee";
 const domain = "o1006021.ingest.sentry.io";
@@ -14,12 +11,6 @@ if (process.env.NODE_ENV === "production") {
   Sentry.init({
     Vue,
     dsn: `https://${publicKey}@${domain}/${projectId}`,
-    integrations: [
-      new Integrations.BrowserTracing({
-        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-        tracingOrigins: ["qrhunt.ml", "qr.zolly.ml", /^\//]
-      })
-    ],
     beforeSend: (event, hint) => {
       if (hint) {
         console.error(hint.originalException || hint.syntheticException);
