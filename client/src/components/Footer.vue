66f161ddb24752660f5faff76638f2efa5498a4b
Victor Winberg <victor.winberg@doktor.se>
2022-07-05 11:04:38 +0200

    feat(app-version): subtle notification

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -2,7 +2,13 @@
   <footer>
     <div class="footer-content">
       <span class="app__name">QR Hunt</span>
-      <span class="app__version">{{ APP_VERSION }}</span>
+      <span class="app__version" @click="viewRelease">
+        {{ APP_VERSION }}
+        <i
+          v-if="localAppVersion !== APP_VERSION"
+          class="far fa-exclamation-circle app__notification"
+        ></i>
+      </span>
     </div>
     <img
       id="qrcode"
@@ -20,13 +26,57 @@ export default Vue.extend({
   name: "Footer",
   data() {
     return {
-      APP_VERSION
+      APP_VERSION,
+      get localAppVersion() {
+        return localStorage.getItem("appVersion");
+      },
+      set localAppVersion(value) {
+        localStorage.setItem("appVersion", value);
+      }
     };
+  },
+  created() {
+    setTimeout(() => {
+      this.localAppVersion = APP_VERSION;
+    }, 60 * 1000);
   },
   methods: {
     ...mapMutations("scan", ["toggleScan"]),
     qrscan() {
       this.toggleScan();
+    },
+    async viewRelease() {
+      this.localAppVersion = APP_VERSION;
+      const url =
+        "//api.github.com/repos/VictorWinberg/qr-hunt/releases/latest";
+      const res = await fetch(url);
+      const json = await res.json();
+      this.$store.commit("popup/setPopup", {
+        title: "Release " + APP_VERSION,
+        subtitle: json.body
+          .replace(/##([^\r\n]+)/g, "<h3>$1</h3>")
+          .replace(/(\*\*|__)(?=\S)([^\r]*?\S[*_]*)\1/g, "<b>$2</b>")
+          .replace(/\r\n\r\n/g, "<br/>")
+          .replace(/\r\n/g, "<br/>")
+          .replace(/<\/h3><br\/>/g, "</h3>"),
+        options: [
+          {
+            name: "Close",
+            type: "disabled",
+            action: async () => {
+              store.commit("popup/setPopup", false);
+            }
+          },
+          {
+            name: "Read More",
+            type: "success",
+            action: async () => {
+              store.commit("popup/setPopup", false);
+              location.href = "//github.com/VictorWinberg/qr-hunt/releases";
+            }
+          }
+        ]
+      });
     }
   }
 });
@@ -63,6 +113,12 @@ footer {
 
 .app__version {
   right: 0;
+}
+
+.app__notification {
+  position: absolute;
+  top: 0.3rem;
+  right: 0.2rem;
 }
 
 #qrcode {
