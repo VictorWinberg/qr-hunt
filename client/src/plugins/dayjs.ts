9bdf8103c35545fd14a3b0fe2b9e1442933d869f
Victor Winberg <victor.m.winberg@gmail.com>
2025-05-19 15:24:26 +0200

    add locale

--- client/src/plugins/dayjs.ts
+++ client/src/plugins/dayjs.ts
@@ -3,10 +3,31 @@ import isoWeek from "dayjs/plugin/isoWeek";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { total } from "./dayjs-custom";
 
+import { EVENT_TYPE } from "@/constants";
+import i18n from "@/plugins/i18n";
+import EventBus from "./event-bus";
+
 dayjs.extend(isoWeek);
 dayjs.extend(relativeTime);
 dayjs.extend(total);
 
 dayjs.Ls.en.weekStart = 1;
+
+const loadDayjsLocale = (locale) => {
+  import(`dayjs/locale/${locale}`)
+    .then(() => {
+      dayjs.locale(locale);
+      console.log(`dayjs locale updated to: ${locale}`);
+    })
+    .catch(() => {
+      console.warn(`Failed to load dayjs locale: ${locale}. Falling back to English.`);
+      dayjs.locale('en');
+    });
+};
+
+loadDayjsLocale(i18n.locale);
+EventBus.$on(EVENT_TYPE.AUTH_CHANGE, ({ locale }) => {
+  if (locale) loadDayjsLocale(locale);
+});
 
 export default dayjs;
